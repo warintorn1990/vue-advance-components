7db6b2b370604c8a01085247077188174f90c76a
warintorn19 <warintorn.wiboonsild@gmail.com>
2019-11-04 19:13:27 +0700

    add

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+               <app-quote >
+    
+                {{title}}
+                <h2>ทดสอบการทำงานของ Scoped</h2>
+               </app-quote>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Quote from './components/Quote'
+    export default {
+        data: function(){
+            return{
+                title: 'ทดสอบ Slotes'
+            };
+        },
+        components: {
+            appQuote: Quote
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ src/components/Quote.vue
@@ -0,0 +1,23 @@
+<template>
+    <div> 
+        <slot></slot>
+    </div>
+</template>
+<script>
+export default {
+    props:{
+        quote: String
+    }
+}
+</script>
+<style scoped>
+    div{
+        border:  1px solid #ccc;
+        box-shadow: 1px 1px 2px black;
+        padding: 30px;
+        margin : 30px auto;
+    }
+    h2{
+        color: red;
+    }
+</style>
